8bd48cd268e17ac1bd515960ef1aefc1d7c3a241
Jessica Wong <jessawong@outlook.com>
2016-01-30 14:54:52 -0800

    modified files

--- ConsoleApplication2/HangmanDirector.cs
+++ ConsoleApplication2/HangmanDirector.cs
@@ -53,52 +53,7 @@ namespace DemoTest
 
         public int record(char character)
         {
-            var currentCounts = new Dictionary<string, List<string>>();
-            listOfGuesses.Add(character);
-            var mostWords = 0;
-            foreach (string word in listOfWords)
-            {
-                string newPattern = "";
-                foreach (char letter in word)
-                {
-                    if (!listOfGuesses.Contains(letter))
-                    {
-                        newPattern += "- ";
-                    }
-                    else
-                    {
-                        newPattern += letter + " ";
-                    }
-                }
-                newPattern = newPattern.Trim();
-                if (!currentCounts.ContainsKey(newPattern))
-                {
-                    currentCounts.Add(newPattern, new List<string>());
-                }
-
-                currentCounts[newPattern].Add(word);
-                if (currentCounts[newPattern].Count > mostWords)
-                {
-                    mostWords = currentCounts[newPattern].Count;
-                    listOfWords = currentCounts[newPattern];
-                    patternString = newPattern;
-                }
-
-            }
-            int times = 0;
-            foreach (char letter in patternString)
-            {
-                if (letter == character)
-                {
-                    times++;
-                }
-            }
-
-            if (times == 0)
-            {
-                max--;
-            }
-            return times;
+            return 0;
 
         }
     }
--- ConsoleApplication2/Resources/HangmanDirector.cs
+++ ConsoleApplication2/Resources/HangmanDirector.cs
@@ -1,60 +1,60 @@
-﻿using System.Collections.Generic;
+﻿//using System.Collections.Generic;
 
-namespace DemoTest
-{
-    public class HangmanDirector
-    {
-        private List<string> listOfWords;
-        private string patternString;
-        private int max;
-        private List<char> listOfGuesses;
+//namespace DemoTest
+//{
+//    public class HangmanDirector
+//    {
+//        private List<string> listOfWords;
+//        private string patternString;
+//        private int max;
+//        private List<char> listOfGuesses;
 
-        public HangmanDirector(List<string> dictionary, int length, int max)
-        {
-            listOfWords = new List<string>();
-            this.max = max;
-            patternString = "-";
-            listOfGuesses = new List<char>();
-            for (int i = 0; i < length; i++)
-            {
-                patternString += " -";
-            }
+//        public HangmanDirector(List<string> dictionary, int length, int max)
+//        {
+//            listOfWords = new List<string>();
+//            this.max = max;
+//            patternString = "-";
+//            listOfGuesses = new List<char>();
+//            for (int i = 0; i < length; i++)
+//            {
+//                patternString += " -";
+//            }
 
-            for (int i = 0; i < dictionary.Count; i++)
-            {
-                if (dictionary[i].Length == length)
-                {
-                    listOfWords.Add(dictionary[i]);
-                }
-            }
+//            for (int i = 0; i < dictionary.Count; i++)
+//            {
+//                if (dictionary[i].Length == length)
+//                {
+//                    listOfWords.Add(dictionary[i]);
+//                }
+//            }
 
 
-        }
+//        }
 
-        public List<string> words()
-        {
-            return listOfWords;
-        }
+//        public List<string> words()
+//        {
+//            return listOfWords;
+//        }
 
-        public int guessesLeft()
-        {
-            return this.max;
-        }
+//        public int guessesLeft()
+//        {
+//            return this.max;
+//        }
 
-        public string pattern()
-        {
-            return patternString;
-        }
+//        public string pattern()
+//        {
+//            return patternString;
+//        }
 
-        public List<char> guesses()
-        {
-            return listOfGuesses;
-        }
+//        public List<char> guesses()
+//        {
+//            return listOfGuesses;
+//        }
 
-        public int record(char character)
-        {
+//        public int record(char character)
+//        {
             
 
-        }
-    }
-}
+//        }
+//    }
+//}
